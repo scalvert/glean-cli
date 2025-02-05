c17dd5fe17df509ca7a38bb3b3a711409479ad7f
Steve Calvert <steve.calvert@gmail.com>
2025-02-04 17:04:39 -0800

    Fixing spinner for search

--- cmd/search.go
+++ cmd/search.go
@@ -8,15 +8,83 @@ import (
 	"text/template"
 	"time"
 
+	"github.com/briandowns/spinner"
 	"github.com/mattn/go-tty"
 	"github.com/scalvert/glean-cli/pkg/config"
 	"github.com/scalvert/glean-cli/pkg/http"
+	"github.com/scalvert/glean-cli/pkg/output"
 	"github.com/spf13/cobra"
 )
 
-var testMode bool
-var testInput string
+// Test mode configuration
+var (
+	testMode  bool
+	testInput string
+)
 
+// Default template for search results
+var defaultTemplate = `{{- range $i, $result := .Results -}}
+{{if $i}}
+
+{{end}}{{add $i 1}} {{formatDatasource $result.Document.Datasource}} | {{gleanBlue $result.Document.Title}}
+{{gleanYellow $result.Document.URL}}
+{{- range $result.Snippets}}
+{{.Text}}{{end}}
+{{- end}}{{if .SuggestedSpellCorrectedQuery}}
+
+Did you mean: {{.SuggestedSpellCorrectedQuery}}?{{end}}{{if .RewrittenQuery}}
+
+Showing results for: {{.RewrittenQuery}}{{end}}`
+
+// Core domain types
+type Document struct {
+	ParentDocument *Document         `json:"parentDocument,omitempty"`
+	Metadata       *DocumentMetadata `json:"metadata"`
+	ID             string            `json:"id"`
+	Datasource     string            `json:"datasource"`
+	DocType        string            `json:"docType"`
+	Title          string            `json:"title"`
+	URL            string            `json:"url"`
+}
+
+type DocumentMetadata struct {
+	Datasource         string                 `json:"datasource"`
+	DatasourceInstance string                 `json:"datasourceInstance"`
+	ObjectType         string                 `json:"objectType"`
+	Container          string                 `json:"container,omitempty"`
+	ContainerId        string                 `json:"containerId,omitempty"`
+	MimeType           string                 `json:"mimeType"`
+	DocumentId         string                 `json:"documentId"`
+	LoggingId          string                 `json:"loggingId"`
+	CreateTime         string                 `json:"createTime"`
+	UpdateTime         string                 `json:"updateTime"`
+	Author             *Person                `json:"author,omitempty"`
+	Owner              *Person                `json:"owner,omitempty"`
+	Visibility         string                 `json:"visibility"`
+	Status             string                 `json:"status,omitempty"`
+	AssignedTo         *Person                `json:"assignedTo,omitempty"`
+	DatasourceId       string                 `json:"datasourceId"`
+	Interactions       map[string]interface{} `json:"interactions"`
+	DocumentCategory   string                 `json:"documentCategory"`
+	CustomData         map[string]interface{} `json:"customData,omitempty"`
+	Shortcuts          []Shortcut             `json:"shortcuts,omitempty"`
+}
+
+type Person struct {
+	Metadata     *PersonMetadata `json:"metadata,omitempty"`
+	Name         string          `json:"name"`
+	ObfuscatedId string          `json:"obfuscatedId"`
+}
+
+type PersonMetadata struct {
+	RelatedDocuments []RelatedDocument `json:"relatedDocuments,omitempty"`
+}
+
+type RelatedDocument struct {
+	// Add fields as needed
+}
+
+// Request types
 type SearchOptions struct {
 	InputDetails      *SearchInputDetails `json:"inputDetails,omitempty"`
 	SessionInfo       *SessionInfo        `json:"sessionInfo,omitempty"`
@@ -39,20 +107,6 @@ type SearchOptions struct {
 
 type SearchInputDetails struct {
 	HasCopyPaste bool `json:"hasCopyPaste,omitempty"`
-}
-
-type Person struct {
-	Metadata     *PersonMetadata `json:"metadata,omitempty"`
-	Name         string          `json:"name"`
-	ObfuscatedId string          `json:"obfuscatedId"`
-}
-
-type PersonMetadata struct {
-	RelatedDocuments []RelatedDocument `json:"relatedDocuments,omitempty"`
-}
-
-type RelatedDocument struct {
-	// Add fields as needed
 }
 
 type RequestOptions struct {
@@ -108,39 +162,6 @@ type SessionInfo struct {
 	TabId                string `json:"tabId,omitempty"`
 }
 
-type Document struct {
-	ParentDocument *Document         `json:"parentDocument,omitempty"`
-	Metadata       *DocumentMetadata `json:"metadata"`
-	ID             string            `json:"id"`
-	Datasource     string            `json:"datasource"`
-	DocType        string            `json:"docType"`
-	Title          string            `json:"title"`
-	URL            string            `json:"url"`
-}
-
-type DocumentMetadata struct {
-	Datasource         string                 `json:"datasource"`
-	DatasourceInstance string                 `json:"datasourceInstance"`
-	ObjectType         string                 `json:"objectType"`
-	Container          string                 `json:"container,omitempty"`
-	ContainerId        string                 `json:"containerId,omitempty"`
-	MimeType           string                 `json:"mimeType"`
-	DocumentId         string                 `json:"documentId"`
-	LoggingId          string                 `json:"loggingId"`
-	CreateTime         string                 `json:"createTime"`
-	UpdateTime         string                 `json:"updateTime"`
-	Author             *Person                `json:"author,omitempty"`
-	Owner              *Person                `json:"owner,omitempty"`
-	Visibility         string                 `json:"visibility"`
-	Status             string                 `json:"status,omitempty"`
-	AssignedTo         *Person                `json:"assignedTo,omitempty"`
-	DatasourceId       string                 `json:"datasourceId"`
-	Interactions       map[string]interface{} `json:"interactions"`
-	DocumentCategory   string                 `json:"documentCategory"`
-	CustomData         map[string]interface{} `json:"customData,omitempty"`
-	Shortcuts          []Shortcut             `json:"shortcuts,omitempty"`
-}
-
 type Shortcut struct {
 	InputAlias     string `json:"inputAlias"`
 	DestinationUrl string `json:"destinationUrl"`
@@ -150,74 +171,6 @@ type Shortcut struct {
 	ViewPrefix     string `json:"viewPrefix"`
 	Alias          string `json:"alias"`
 	Title          string `json:"title"`
-}
-
-var defaultTemplate = `{{- range $i, $result := .Results -}}
-{{if $i}}
-
-{{end}}{{add $i 1}} {{formatDatasource $result.Document.Datasource}} | {{gleanBlue $result.Document.Title}}
-{{gleanYellow $result.Document.URL}}
-{{- range $result.Snippets}}
-{{.Text}}{{end}}
-{{- end}}{{if .SuggestedSpellCorrectedQuery}}
-
-Did you mean: {{.SuggestedSpellCorrectedQuery}}?{{end}}{{if .RewrittenQuery}}
-
-Showing results for: {{.RewrittenQuery}}{{end}}`
-
-func NewCmdSearch() *cobra.Command {
-	opts := &SearchOptions{
-		RequestOptions: &RequestOptions{
-			FacetBucketSize: 10,
-			ResponseHints:   []string{"RESULTS", "QUERY_METADATA"},
-		},
-	}
-
-	cmd := &cobra.Command{
-		Use:   "search [query]",
-		Short: "Search for content in your Glean instance",
-		Long: `Search for content in your Glean instance.
-
-You can search for any content that is indexed in your Glean instance.
-The results will be displayed in a formatted list by default.
-
-Example:
-  glean search "vacation policy"
-  glean search --page-size 20 "engineering docs"
-  glean search --template "{{range .Results}}{{.Title}}\n{{end}}" "meeting notes"`,
-		Args: cobra.ExactArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			opts.Query = args[0]
-			return runSearch(cmd, opts)
-		},
-	}
-
-	// Basic search options
-	cmd.Flags().IntVarP(&opts.PageSize, "page-size", "n", 10, "Number of results per page")
-	cmd.Flags().BoolVar(&opts.DisableSpellcheck, "disable-spellcheck", false, "Disable spellcheck suggestions")
-	cmd.Flags().IntVar(&opts.MaxSnippetSize, "max-snippet-size", 200, "Maximum length of result snippets")
-	cmd.Flags().IntVar(&opts.TimeoutMillis, "timeout", 30000, "Request timeout in milliseconds")
-
-	// Output formatting
-	cmd.Flags().StringVarP(&opts.Template, "template", "t", "", "Go template for formatting results")
-	cmd.Flags().StringVarP(&opts.OutputFormat, "output", "o", "text", "Output format: text, json")
-	cmd.Flags().BoolVar(&opts.NoColor, "no-color", false, "Disable colorized output")
-
-	// Filtering options
-	cmd.Flags().StringSliceP("datasource", "d", nil, "Filter by datasource (can be specified multiple times)")
-	cmd.Flags().StringSliceP("type", "y", nil, "Filter by document type (can be specified multiple times)")
-	cmd.Flags().StringSliceP("person", "p", nil, "Filter by person email (can be specified multiple times)")
-	cmd.Flags().StringSlice("tab", nil, "Filter by result tab IDs (can be specified multiple times)")
-
-	// Advanced options
-	cmd.Flags().Bool("disable-query-autocorrect", false, "Disable automatic query corrections")
-	cmd.Flags().Bool("fetch-all-datasource-counts", false, "Return result counts for all supported datasources")
-	cmd.Flags().Bool("query-overrides-facet-filters", false, "Let query operators override facet filters")
-	cmd.Flags().Bool("return-llm-content", false, "Return expanded content for LLM usage")
-	cmd.Flags().StringSlice("response-hints", []string{"RESULTS", "QUERY_METADATA"}, "Response hints (RESULTS, QUERY_METADATA, etc)")
-	cmd.Flags().Int("facet-bucket-size", 10, "Maximum number of facet buckets to return")
-
-	return cmd
 }
 
 // Additional response types
@@ -236,8 +189,9 @@ type FacetResult struct {
 }
 
 type FacetBucket struct {
-	Value string `json:"value"`
-	Count int    `json:"count"`
+	Value       interface{} `json:"value"`
+	DisplayName string      `json:"displayName,omitempty"`
+	Count       int         `json:"count"`
 }
 
 type GeneratedQna struct {
@@ -327,17 +281,90 @@ type SearchMetadata struct {
 	ObjectType string  `json:"objectType,omitempty"`
 }
 
+// NewCmdSearch creates and returns the search command
+func NewCmdSearch() *cobra.Command {
+	opts := &SearchOptions{
+		RequestOptions: &RequestOptions{
+			FacetBucketSize: 10,
+			ResponseHints:   []string{"RESULTS", "QUERY_METADATA"},
+		},
+	}
+
+	cmd := &cobra.Command{
+		Use:   "search [query]",
+		Short: "Search for content in your Glean instance",
+		Long: `Search for content in your Glean instance.
+
+You can search for any content that is indexed in your Glean instance.
+The results will be displayed in a formatted list by default.
+
+Example:
+  glean search "vacation policy"
+  glean search --page-size 20 "engineering docs"
+  glean search --template "{{range .Results}}{{.Title}}\n{{end}}" "meeting notes"`,
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			opts.Query = args[0]
+			return runSearch(cmd, opts)
+		},
+	}
+
+	// Basic search options
+	cmd.Flags().IntVarP(&opts.PageSize, "page-size", "n", 10, "Number of results per page")
+	cmd.Flags().BoolVar(&opts.DisableSpellcheck, "disable-spellcheck", false, "Disable spellcheck suggestions")
+	cmd.Flags().IntVar(&opts.MaxSnippetSize, "max-snippet-size", 200, "Maximum length of result snippets")
+	cmd.Flags().IntVar(&opts.TimeoutMillis, "timeout", 30000, "Request timeout in milliseconds")
+
+	// Output formatting
+	cmd.Flags().StringVarP(&opts.Template, "template", "t", "", "Go template for formatting results")
+	cmd.Flags().StringVarP(&opts.OutputFormat, "output", "o", "text", "Output format: text, json")
+	cmd.Flags().BoolVar(&opts.NoColor, "no-color", false, "Disable colorized output")
+
+	// Filtering options
+	cmd.Flags().StringSliceP("datasource", "d", nil, "Filter by datasource (can be specified multiple times)")
+	cmd.Flags().StringSliceP("type", "y", nil, "Filter by document type (can be specified multiple times)")
+	cmd.Flags().StringSliceP("person", "p", nil, "Filter by person email (can be specified multiple times)")
+	cmd.Flags().StringSlice("tab", nil, "Filter by result tab IDs (can be specified multiple times)")
+
+	// Advanced options
+	cmd.Flags().Bool("disable-query-autocorrect", false, "Disable automatic query corrections")
+	cmd.Flags().Bool("fetch-all-datasource-counts", false, "Return result counts for all supported datasources")
+	cmd.Flags().Bool("query-overrides-facet-filters", false, "Let query operators override facet filters")
+	cmd.Flags().Bool("return-llm-content", false, "Return expanded content for LLM usage")
+	cmd.Flags().StringSlice("response-hints", []string{"RESULTS", "QUERY_METADATA"}, "Response hints (RESULTS, QUERY_METADATA, etc)")
+	cmd.Flags().Int("facet-bucket-size", 10, "Maximum number of facet buckets to return")
+
+	return cmd
+}
+
+// runSearch executes the search command with the given options
 func runSearch(cmd *cobra.Command, opts *SearchOptions) error {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		return fmt.Errorf("failed to load config: %w", err)
+	}
+
+	client, err := http.NewClient(cfg)
+	if err != nil {
+		return err
+	}
+
+	// Start spinner
+	spin := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
+	spin.Prefix = "Searching Company Knowledge "
+	spin.Start()
+	defer spin.Stop()
+
 	// Handle facet filters
-	if datasources, err := cmd.Flags().GetStringSlice("datasource"); err == nil && len(datasources) > 0 {
+	if datasources, flagErr := cmd.Flags().GetStringSlice("datasource"); flagErr == nil && len(datasources) > 0 {
 		addFacetFilter(opts, "datasource", datasources)
 	}
-	if types, err := cmd.Flags().GetStringSlice("type"); err == nil && len(types) > 0 {
+	if types, flagErr := cmd.Flags().GetStringSlice("type"); flagErr == nil && len(types) > 0 {
 		addFacetFilter(opts, "type", types)
 	}
 
 	// Handle person filters
-	if people, err := cmd.Flags().GetStringSlice("person"); err == nil && len(people) > 0 {
+	if people, flagErr := cmd.Flags().GetStringSlice("person"); flagErr == nil && len(people) > 0 {
 		opts.People = make([]Person, len(people))
 		for i, email := range people {
 			opts.People[i] = Person{
@@ -347,7 +374,7 @@ func runSearch(cmd *cobra.Command, opts *SearchOptions) error {
 	}
 
 	// Handle result tab filters
-	if tabs, err := cmd.Flags().GetStringSlice("tab"); err == nil && len(tabs) > 0 {
+	if tabs, flagErr := cmd.Flags().GetStringSlice("tab"); flagErr == nil && len(tabs) > 0 {
 		opts.ResultTabIds = tabs
 	}
 
@@ -360,47 +387,48 @@ func runSearch(cmd *cobra.Command, opts *SearchOptions) error {
 	opts.RequestOptions.TimezoneOffset = getTimezoneOffset()
 
 	// Set facet bucket size
-	if size, err := cmd.Flags().GetInt("facet-bucket-size"); err == nil {
+	if size, flagErr := cmd.Flags().GetInt("facet-bucket-size"); flagErr == nil {
 		opts.RequestOptions.FacetBucketSize = size
 	}
 
 	// Set response hints
-	if hints, err := cmd.Flags().GetStringSlice("response-hints"); err == nil {
+	if hints, flagErr := cmd.Flags().GetStringSlice("response-hints"); flagErr == nil {
 		opts.RequestOptions.ResponseHints = hints
 	}
 
 	// Set boolean flags
-	if disable, err := cmd.Flags().GetBool("disable-query-autocorrect"); err == nil {
+	if disable, flagErr := cmd.Flags().GetBool("disable-query-autocorrect"); flagErr == nil {
 		opts.RequestOptions.DisableQueryAutocorrect = disable
 	}
-	if fetch, err := cmd.Flags().GetBool("fetch-all-datasource-counts"); err == nil {
+	if fetch, flagErr := cmd.Flags().GetBool("fetch-all-datasource-counts"); flagErr == nil {
 		opts.RequestOptions.FetchAllDatasourceCounts = fetch
 	}
-	if override, err := cmd.Flags().GetBool("query-overrides-facet-filters"); err == nil {
+	if override, flagErr := cmd.Flags().GetBool("query-overrides-facet-filters"); flagErr == nil {
 		opts.RequestOptions.QueryOverridesFacetFilters = override
 	}
-	if llm, err := cmd.Flags().GetBool("return-llm-content"); err == nil {
+	if llm, flagErr := cmd.Flags().GetBool("return-llm-content"); flagErr == nil {
 		opts.RequestOptions.ReturnLlmContentOverSnippets = llm
 	}
 
-	cfg, err := config.LoadConfig()
-	if err != nil {
-		return fmt.Errorf("failed to load config: %w", err)
-	}
-
-	client, err := http.NewClient(cfg)
+	// Perform search
+	resp, err := performSearch(client, opts, "", "")
 	if err != nil {
 		return err
 	}
 
-	// Initial search request
-	response, err := performSearch(client, opts, "", "")
-	if err != nil {
-		return err
-	}
+	// Stop spinner before writing output
+	spin.Stop()
 
+	// Format and write output
 	if opts.OutputFormat == "json" {
-		return json.NewEncoder(cmd.OutOrStdout()).Encode(response)
+		jsonBytes, marshalErr := json.Marshal(resp)
+		if marshalErr != nil {
+			return fmt.Errorf("error marshaling JSON: %w", marshalErr)
+		}
+		return output.Write(cmd.OutOrStdout(), jsonBytes, output.Options{
+			NoColor: opts.NoColor,
+			Format:  "json",
+		})
 	}
 
 	// Use template for output
@@ -430,13 +458,13 @@ func runSearch(cmd *cobra.Command, opts *SearchOptions) error {
 	}
 
 	// Print initial results
-	err = t.Execute(cmd.OutOrStdout(), response)
+	err = t.Execute(cmd.OutOrStdout(), resp)
 	if err != nil {
 		return fmt.Errorf("error executing template: %w", err)
 	}
 
 	// Handle pagination if needed
-	for response.HasMoreResults {
+	for resp.HasMoreResults {
 		fmt.Fprint(cmd.OutOrStdout(), "\n\nPress 'q' to quit, any other key to load more results...")
 
 		if testMode {
@@ -471,12 +499,12 @@ func runSearch(cmd *cobra.Command, opts *SearchOptions) error {
 			}
 		}
 
-		response, err = performSearch(client, opts, response.Cursor, response.TrackingToken)
+		resp, err = performSearch(client, opts, resp.Cursor, resp.TrackingToken)
 		if err != nil {
 			return err
 		}
 
-		err = t.Execute(cmd.OutOrStdout(), response)
+		err = t.Execute(cmd.OutOrStdout(), resp)
 		if err != nil {
 			return fmt.Errorf("error executing template: %w", err)
 		}
@@ -485,7 +513,7 @@ func runSearch(cmd *cobra.Command, opts *SearchOptions) error {
 	return nil
 }
 
-// getTimezoneOffset returns the local timezone offset in minutes from UTC
+// Helper functions
 func getTimezoneOffset() int {
 	_, offset := time.Now().Zone()
 	return offset / 60 // Convert seconds to minutes
@@ -503,6 +531,23 @@ func addFacetFilter(opts *SearchOptions, fieldName string, values []string) {
 		}
 	}
 	opts.RequestOptions.FacetFilters = append(opts.RequestOptions.FacetFilters, filter)
+}
+
+func formatDatasource(s string) string {
+	if s == "nonindexedshortcut" {
+		return "GoLink"
+	}
+
+	words := []rune(s)
+	if len(words) > 0 {
+		words[0] = []rune(strings.ToUpper(string(words[0])))[0]
+	}
+	for i := 1; i < len(words); i++ {
+		if words[i-1] == ' ' {
+			words[i] = []rune(strings.ToUpper(string(words[i])))[0]
+		}
+	}
+	return string(words)
 }
 
 func performSearch(client http.Client, opts *SearchOptions, cursor, trackingToken string) (*SearchResponse, error) {
@@ -560,21 +605,4 @@ func performSearch(client http.Client, opts *SearchOptions, cursor, trackingToke
 	}
 
 	return &searchResp, nil
-}
-
-func formatDatasource(s string) string {
-	if s == "nonindexedshortcut" {
-		return "GoLink"
-	}
-
-	words := []rune(s)
-	if len(words) > 0 {
-		words[0] = []rune(strings.ToUpper(string(words[0])))[0]
-	}
-	for i := 1; i < len(words); i++ {
-		if words[i-1] == ' ' {
-			words[i] = []rune(strings.ToUpper(string(words[i])))[0]
-		}
-	}
-	return string(words)
 }
